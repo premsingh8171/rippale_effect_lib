bfd6191e0cabe197fe5febea9b725a8b7c499129
Er. prem singh daksha <46933759+premsingh8171@users.noreply.github.com>
2021-01-13 15:07:09 +0530

    update code

--- app/src/main/java/com/premsinghdaksha/rippaleeffectdemo/MainActivity.java
+++ app/src/main/java/com/premsinghdaksha/rippaleeffectdemo/MainActivity.java
@@ -1,26 +1,25 @@
 package com.premsinghdaksha.rippaleeffectdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.premsinghdaksha.rippale_effect_lib.RippleEffectView;
 
 public class MainActivity extends AppCompatActivity {
     RippleEffectView effectView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         effectView = findViewById(R.id.rippaleEffect_Id);
         effectView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(getApplicationContext(),"RippaleEffect",Toast.LENGTH_LONG);
+                Toast.makeText(getApplicationContext(), "RippaleEffect", Toast.LENGTH_LONG);
             }
         });
     }
